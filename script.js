c5bf6a9574651bea2d6e5d7be30793dccc42b738
NychBo <dark2dreamer2@gmail.com>
2026-03-12 17:23:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,95 +1,52 @@
+let currentIndex = 0
+let images = []
+
 function setLang(lang){
 
 document.querySelectorAll("[data-ua]").forEach(el=>{
-el.innerText=el.dataset[lang]
+el.innerText = el.dataset[lang]
 })
 
 }
 
-window.onload=function(){
-setLang("ua")
-initGallery()
-}
+function openViewer(img){
 
+images = Array.from(document.querySelectorAll(".gallery img"))
 
+currentIndex = images.indexOf(img)
 
-let images=[]
-let index=0
-let viewer
-let viewerImg
+document.getElementById("viewer").style.display="flex"
 
-
-
-function initGallery(){
-
-images=[...document.querySelectorAll(".gallery img")]
-
-viewer=document.getElementById("viewer")
-viewerImg=document.getElementById("viewer-img")
-
-images.forEach((img,i)=>{
-
-img.onclick=function(){
-
-index=i
-viewer.style.display="flex"
-viewerImg.src=this.src
+document.getElementById("viewer-img").src = img.src
 
 }
 
-})
+function closeViewer(){
 
-
-document.querySelector(".close").onclick=function(){
-
-viewer.style.display="none"
+document.getElementById("viewer").style.display="none"
 
 }
 
+function nextImg(){
 
+currentIndex++
 
-document.addEventListener("keydown",function(e){
+if(currentIndex >= images.length){
+currentIndex = 0
+}
 
-if(viewer.style.display!=="flex")return
-
-if(e.key==="ArrowRight")next()
-
-if(e.key==="ArrowLeft")prev()
-
-})
-
-
-
-let startX=0
-
-viewer.addEventListener("touchstart",e=>{
-startX=e.touches[0].clientX
-})
-
-viewer.addEventListener("touchend",e=>{
-
-let endX=e.changedTouches[0].clientX
-
-if(startX-endX>50)next()
-
-if(endX-startX>50)prev()
-
-})
+document.getElementById("viewer-img").src = images[currentIndex].src
 
 }
 
+function prevImg(){
 
+currentIndex--
 
-function next(){
-
-index=(index+1)%images.length
-viewerImg.src=images[index].src
-
+if(currentIndex < 0){
+currentIndex = images.length-1
 }
 
-function prev(){
-
-index=(index-1+images.length)%images.length
-viewerImg.src=images[index].src
+document.getElementById("viewer-img").src = images[currentIndex].src
 
 }
